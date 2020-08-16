2ac3069ae1cec8d93ee30f41b17e9e0086e2d73d
shin-ee-chen <xchen_2020@hotmail.com>
2020-08-16 20:50:43 +0800

    first commit

---
+++ BiDAF-TF2/__init__.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# _*_ coding: utf-8 _*_
+# @Time    : 2020/8/15 5:30 下午 
+# @Author  : Roger 
+# @Version : V 0.1
+# @Email   : 550997728@qq.com
+# @File    : __init__.py.py
+
